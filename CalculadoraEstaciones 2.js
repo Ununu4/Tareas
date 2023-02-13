c5bba92a3ff26cab4d5c66cc6e890d4cbfe0abd9
Otto F. Guzman <124011477+Ununu4@users.noreply.github.com>
2023-02-13 14:27:42 -0600

    Add files via upload

---
+++ CalculadoraEstaciones 2.js
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2023 by Ununu4 (http://jsbin.com/tediles/1/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+
+<script id="jsbin-javascript">
+
+var mes = Number(prompt('Ingrese el numero de un mes'))
+if  (mes > 12){
+alert('valor invalido')}
+
+else
+
+
+switch(mes) {
+
+  case 1:
+alert('es invierno')
+break;
+case 2:
+alert('es invierno')
+break
+case 3:
+alert('es primavera')
+break
+case 4:
+alert('es primavera')
+break
+case 5:
+alert('es primavera')
+break
+case 6:
+alert('es verano')
+break
+case 7:
+alert('es verano')
+break
+case 8:
+alert('es verano')
+break
+case 9:
+alert('es otoño')
+break
+case 10:
+alert('es otoño')
+break 
+case 11:
+alert('es otoño')
+break
+case 12:
+alert('es invierno');
+break
+
+}{
+  alert("Otto Fernando Guzman // Carnet  23002638 ");
+}
+
+
+</script>
+</body>
+</html>
